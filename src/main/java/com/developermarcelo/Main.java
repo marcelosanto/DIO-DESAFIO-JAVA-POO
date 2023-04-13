0a37d8776ae6ea9c527376f5c0db0f56758faf38
Marcelo Santos <rocha.mer21@gmail.com>
2023-04-13 12:11:55 -0300

    feat: terminado

--- src/main/java/com/developermarcelo/Main.java
+++ src/main/java/com/developermarcelo/Main.java
@@ -1,6 +1,8 @@
 package com.developermarcelo;
 
+import com.developermarcelo.desafio.dominio.BootCamp;
 import com.developermarcelo.desafio.dominio.Curso;
+import com.developermarcelo.desafio.dominio.Dev;
 import com.developermarcelo.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -12,20 +14,51 @@ public class Main {
         curso.setDescricao("Pequeno desafio poo dio java bootcamp");
         curso.setCargaHoraria(80);
 
-        System.out.println(curso);
+        //System.out.println(curso);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Desafio Kotlin POO");
         curso2.setDescricao("Pequeno desafio poo dio Kotlin bootcamp");
-        curso2.setCargaHoraria(180);
+        curso2.setCargaHoraria(75);
 
-        System.out.println(curso2);
+        //System.out.println(curso2);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Hoje");
         mentoria.setDescricao("Acontencendo agora");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+        //System.out.println(mentoria);
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java Developer");
+        bootCamp.setDescricao("Descriçao Bootcamp Java Developer");
+        bootCamp.getConteudos().add(curso);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devMarcelo = new Dev();
+        devMarcelo.setNome("Marcelo");
+        devMarcelo.inscreverBootCamp(bootCamp);
+        System.out.println("Conteudos Inscritos DevMarcelo: " + devMarcelo.getConteudosInscritos());
+        devMarcelo.progredir();
+        System.out.println("Conteudos Inscritos DevAlice: " + devMarcelo.getConteudosInscritos());
+        System.out.println("Conteudos Conluidos DevAlice: " + devMarcelo.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcelo.calcularXp());
+
+        System.out.println(" ");
+        System.out.println("-/-/-/-/-/-/-/-/-/-/-/-");
+        System.out.println(" ");
+
+        Dev devAlice = new Dev();
+        devAlice.setNome("Alice");
+        devAlice.inscreverBootCamp(bootCamp);
+        System.out.println("Conteudos Inscritos DevAlice: " + devAlice.getConteudosInscritos());
+        devAlice.progredir();
+        devAlice.progredir();
+        System.out.println("Conteudos Inscritos DevAlice: " + devAlice.getConteudosInscritos());
+        System.out.println("Conteudos Conluidos DevAlice: " + devAlice.getConteudosConcluidos());
+        System.out.println("XP: " + devAlice.calcularXp());
+
     }
 }
--- src/main/java/com/developermarcelo/desafio/dominio/Dev.java
+++ src/main/java/com/developermarcelo/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package com.developermarcelo.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,12 +11,23 @@ public class Dev {
     private String nome;
 
     public void inscreverBootCamp(BootCamp bootCamp) {
+        this.conteudosInscritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
-    public void calcularXp() {
+    public double calcularXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
     }
 
     public Set<Conteudo> getConteudosInscritos() {
