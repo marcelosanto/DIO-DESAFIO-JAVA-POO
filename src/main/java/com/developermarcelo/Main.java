66a805b3be70e0e855a64d107725fda6035fe157
Marcelo Santos <rocha.mer21@gmail.com>
2023-04-13 11:15:22 -0300

    feat: create Class Conteudo

--- src/main/java/com/developermarcelo/Main.java
+++ src/main/java/com/developermarcelo/Main.java
@@ -1,7 +1,31 @@
 package com.developermarcelo;
 
+import com.developermarcelo.desafio.dominio.Curso;
+import com.developermarcelo.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Desafio Java POO");
+        curso.setDescricao("Pequeno desafio poo dio java bootcamp");
+        curso.setCargaHoraria(80);
+
+        System.out.println(curso);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Desafio Kotlin POO");
+        curso2.setDescricao("Pequeno desafio poo dio Kotlin bootcamp");
+        curso2.setCargaHoraria(180);
+
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Hoje");
+        mentoria.setDescricao("Acontencendo agora");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
     }
 }
--- src/main/java/com/developermarcelo/desafio/dominio/Curso.java
+++ src/main/java/com/developermarcelo/desafio/dominio/Curso.java
@@ -1,25 +1,8 @@
 package com.developermarcelo.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -30,10 +13,15 @@ public class Curso {
     }
 
     @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    @Override
     public String toString() {
         return "Curso { " +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
